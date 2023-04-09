1be0ae151c8a28b06096b14a6ffda7588edf554c
linuxchallenge <43291948+linuxchallenge@users.noreply.github.com>
2023-04-09 14:00:55 +0530

    Move from nsepy to yahoo finance.
    Move from nsepy to yahoo finance.

---
+++ DailySchedule/yahooFin.py
@@ -0,0 +1,33 @@
+import yfinance as yf
+import pandas as pd
+from datetime import datetime, timedelta
+
+
+class yahooFin_api(object):
+
+    def __init__(self):
+        pass
+        # create object of call
+
+    def OHLCHistoricData(self, symbol, fdate, todate):
+        try:
+            yestday_date = todate - timedelta(days=1)
+            symbol = symbol + ".NS"
+            print(symbol)
+            data_fut = yf.download(tickers=symbol, start=fdate, end=yestday_date)
+            data_fut = data_fut[['Open', 'High', 'Low', 'Close', 'Volume']]
+
+            return data_fut
+        except Exception as e:
+            print("Historic API failed: {}".format(e))
+
+# nifty_200_df = pd.read_csv('ind_nifty200list.csv')
+# x = yahooFin_api()
+# Iterate all rows using DataFrame.iterrows()
+# current = datetime.now()
+# ten_days_before = current - timedelta(days=20)
+
+# for index, row in nifty_200_df.iterrows():
+#    print(row["Symbol"])
+#    df = x.OHLCHistoricData(row["Symbol"], ten_days_before, current)
+#    print(df)
