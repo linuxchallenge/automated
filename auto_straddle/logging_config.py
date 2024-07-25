a86ffffcc5739cd629638fdf3bd62a7e549497f5
Adithya K <adithya.k@arable.com>
2024-07-25 17:30:42 +0530

    Change logger

--- auto_straddle/logging_config.py
+++ auto_straddle/logging_config.py
@@ -7,7 +7,9 @@ import logging.config
 from datetime import datetime
 
 def setup_logging(log_level=logging.INFO):
+    # Generate log file name with the current date
     log_file_name = f"/tmp/auto_trade_{datetime.now().strftime('%Y-%m-%d')}.log"
+    
     logging_config = {
         'version': 1,
         'disable_existing_loggers': False,
@@ -17,21 +19,16 @@ def setup_logging(log_level=logging.INFO):
             },
         },
         'handlers': {
-            'console': {
-                'level': log_level,
-                'class': 'logging.StreamHandler',
-                'formatter': 'standard',
-            },
             'file': {
                 'level': log_level,
                 'class': 'logging.FileHandler',
                 'formatter': 'standard',
-                'filename': log_file_name
+                'filename': log_file_name,  # Log file path with date
             },
         },
         'loggers': {
             '': {
-                'handlers': ['console', 'file'],
+                'handlers': ['file'],
                 'level': log_level,
                 'propagate': True
             },
