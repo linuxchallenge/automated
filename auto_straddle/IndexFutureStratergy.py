4ef22acd0bd51a10856fb17d3a5419f53202d5c1
linuxchallenge <43291948+linuxchallenge@users.noreply.github.com>
2026-03-19 16:17:16 +0530

    Index future start at 9:17.

--- auto_straddle/IndexFutureStratergy.py
+++ auto_straddle/IndexFutureStratergy.py
@@ -455,7 +455,7 @@ class IndexFutureStratergy:
 
             current_time_dt = datetime.now().time()
 
-            if current_time_dt < time(9, 15):
+            if current_time_dt < time(9, 17):
                 return
 
             # return if time is more than 3:30
